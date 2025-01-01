bad4e92cc4e298715a92df49178b851848f54ac4
JoannVetter <joann.vetter.dev@proton.me>
2025-01-02 00:44:42 +0100

    fix (main): removing useless routes copied from the fastAPI tutorial

--- src/main.py
+++ src/main.py
@@ -46,19 +46,5 @@ async def login_for_access_token(
     return Token(access_token=access_token, token_type="bearer")
 
 
-@app.get("/users/me/", response_model=PowerUser)
-async def read_users_me(
-        current_user: Annotated[PowerUser, Depends(get_current_active_user)],
-):
-    return current_user
-
-
-@app.get("/users/me/items/")
-async def read_own_items(
-        current_user: Annotated[PowerUser, Depends(get_current_active_user)],
-):
-    return [{"item_id": "Foo", "owner": current_user.username}]
-
-
 if __name__ == "__main__":
     uvicorn.run(app)
